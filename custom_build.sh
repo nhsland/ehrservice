72375c9e33aee4e4fc7ce892e0fe6535fdd5e9c9
Rob Dyke <robdyke@gmail.com>
2018-03-04 01:19:00 +0000

    Gradle

--- custom_build.sh
+++ custom_build.sh
@@ -3,4 +3,3 @@
 echo 'running custom mvn by ignoring failed tests'
 
 mvn clean install site -Dmaven.test.failure.ignore=true
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
