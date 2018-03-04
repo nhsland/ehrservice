6ba5580081a8e8700e716ad4240756a1291b6c5f
Rob Dyke <robdyke@gmail.com>
2018-03-04 01:05:09 +0000

    SonarQube Scanner for Maven

--- custom_build.sh
+++ custom_build.sh
@@ -3,3 +3,4 @@
 echo 'running custom mvn by ignoring failed tests'
 
 mvn clean install site -Dmaven.test.failure.ignore=true
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
